dcf3d155bf9ec4f92fe78eed65f64cdd8ea72ce0
harsh <harshlohana38@gmail.com>
2021-09-17 21:31:07 +0530

    2 commit

--- Main.java
+++ Main.java
@@ -3,9 +3,7 @@ import java.util.*;
 
 class Main{
 public static void main(String[] args){
-int a=Integer.parseInt(args[0]);
-int b=Integer.parseInt(args[1]);
-int c=a+b;
-System.out.println(c);
+System.out.println("Hello");
+System.out.println("Hello1");
 }
 }
